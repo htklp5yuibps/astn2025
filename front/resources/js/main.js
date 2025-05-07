5b39a77e4efa61278cbc9c2267d7ec72b381925f
Vladislav <v1ktukc9fdbp@gmail.com>
2025-05-07 13:22:40 +0300

    fix: added CORSFilter to server-side

--- front/resources/js/main.js
+++ front/resources/js/main.js
@@ -1,12 +1,12 @@
-window.onload(() => {
+window.onload = () => {
     const pageContentData = document.querySelector(".page-content-data");
 
     const request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:8080/astn2025/users');
 
-    request.onload(() => {
+    request.onload = () => {
         pageContentData.textContent = request.responseText;
-    });
+    };
 
     request.send();
-});
+};
